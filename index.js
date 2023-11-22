88e74710a08dea17c56e54e015434060a01e0150
Diana Discher <dianadischer@dianas-mbp.myfiosgateway.com>
2023-11-21 23:06:43 -0500

    Added questions and skeleton for markdown generator, and write to file function

--- index.js
+++ index.js
@@ -1,13 +1,80 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    //Title
+    {
+        type: 'input',
+        message: 'What is the title of your README?',
+        name: 'title'
+    },
+
+    //Description
+    {
+        type: 'input',
+        message: 'In a few sentences, describe your project.',
+        name: 'description'
+    },
+
+    //Installation
+    {
+        type: 'input',
+        message: 'How would a user install your project?',
+        name: 'installation'
+    },
+
+    //Usage
+    {
+        type: 'input',
+        message: 'Walk through the process of using your project.',
+        name: 'usage'
+    },
+
+    //License
+    {
+        type: 'list',
+        message: 'Which license are you using?',
+        name: 'license',
+        choices: ['Apache 2.0 License', 'GNU GPL v3', 'MIT License', 'BSD 2-Clause License', 'BSD 3-Clause License', 'Boost Software License 1.0', 
+        'CC0', 'Eclipse Public License 2.0', 'GNU AGPL v3', 'GNU GPL v2', 'GNU LGPL v3', 'Mozilla Public License 2.0', 'The Unlicense']
+    },
+    
+    //Contributing
+    {
+        type: 'input',
+        message: 'List any contributors to the project.',
+        name: 'contributors'
+    },
+    
+    //Tests 
+    {
+        type: 'input',
+        message: 'What are some tests for your project?',
+        name: 'tests'
+    },    
+
+    //Questions
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(`${fileName}.md`, data, (err) =>
+    err ? console.error(err) : console.log('README generated!')
+);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer 
+        .prompt(questions)
+        .then((data) =>
+            writeToFile(data.title, generateMarkdown(data))
+        );
+    
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -8,11 +8,40 @@ function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  return `The license for this project is: ${license}`
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+${renderLicenseBadge(data.license)}
+## Desciption
+${data.description}
+
+##Table of Contents
+*[Installation](#Installation)  
+*[Usage](#Usage)  
+*[Contribution Guidelines](#Contribution&nbsp;Guidelines)  
+*[Testing](#Testing)  
+*[Questions](#Questions) 
+
+##Installation
+${data.installation}    
+
+##Usage
+${data.usage}
+    
+##License
+${renderLicenseSection(data.license)}
+
+##Contributing
+${data.contributors}
+    
+##Tests 
+${data.tests}
+    
+##Questions
 
 `;
 }
